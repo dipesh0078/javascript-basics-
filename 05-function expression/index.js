b49ddf15e202f3dba1b091810ba93a3b2113048f
Dipesh Thapa <83170697+dipesh0078@users.noreply.github.com>
2023-06-14 23:16:41 +0545

    Delete 05-function expression directory

--- 05-function expression/index.js
+++
@@ -1,9 +0,0 @@
-let count=0;
-document.getElementById("decreasebtn").onclick=function(){
-    count=count-1;
-    document.getElementById("myLabel").innerHTML=count;
-}
-document.getElementById("increasebtn").onclick=function(){
-    count=count+1;
-    document.getElementById("myLabel").innerHTML=count;
-}
