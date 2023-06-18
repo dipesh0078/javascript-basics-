ccb66ce9398befe12dda71baccec4e0e87889612
Dipesh Thapa <83170697+dipesh0078@users.noreply.github.com>
2023-06-18 22:20:19 +0545

    Add files via upload

---
+++ 27-ES6 module.js
@@ -0,0 +1,7 @@
+import {PI,getArea,getCircumference} from "./math_util.js";
+
+console.log(PI);
+let circumference=getCircumference(10);
+console.log(circumference);
+let area=getArea(10);
+console.log(area);
